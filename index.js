790b25ec991df361c507ca6a702227928eaf3f6d
Marty Chouette <martychouette@gmail.com>
2023-04-14 08:42:48 -0700

    audio update

--- index.js
+++ index.js
@@ -72,24 +72,7 @@ function menuFunction() {
   }
 }
 
-function onlyPlayOneIn(container) {
-  container.addEventListener(
-    "play",
-    function (event) {
-      audio_elements = container.getElementsByTagName("audio");
-      for (i = 0; i < audio_elements.length; i++) {
-        audio_element = audio_elements[i];
-        if (audio_element !== event.target) {
-          audio_element.pause();
-        }
-      }
-    },
-    true
-  );
-}
 
-document.addEventListener("DOMContentLoaded", function () {
-  onlyPlayOneIn(document.body);
-});
 
-container.getElementById("audio");
+
+ 
